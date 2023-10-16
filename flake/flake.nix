6a382df10a8e5d56625a5beaa109a7c1fc93f798
Ernests Kuznecovs <ernests.kuznecovs@channable.com>
2023-10-16 02:00:17 +0200

    Remove flake-compat from flake deps.

--- flake/flake.nix
+++ flake/flake.nix
@@ -2,13 +2,11 @@
   description = "Pinned dependencies for Overlay.";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
   inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.flake-compat.url = "github:edolstra/flake-compat";
   
   outputs =
     { self
     , nixos-hardware
-    , home-manager
-    , flake-compat}: {
+    , home-manager }: {
     lib.overlay = import ../.
       { inherit
         nixos-hardware
