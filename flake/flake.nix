95987a9447442b07f12ab8842418fdc37cf027c9
ernestkz <ernestkuznecovs@gmail.com>
2023-12-22 12:48:51 +0100

    Clean up flake

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "Pinned dependencies for Overlay.";
+  description                 = "Pinned dependencies for Overlay.";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
-  inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.disko.url = "github:nix-community/disko";
-  inputs.impermanence.url = "github:nix-community/impermanence";
+  inputs.nixos-hardware.url   = "github:NixOS/nixos-hardware";
+  inputs.home-manager.url     = "github:nix-community/home-manager";
+  inputs.disko.url            = "github:nix-community/disko";
+  inputs.impermanence.url     = "github:nix-community/impermanence";
 
   outputs =
     { self
@@ -13,10 +13,11 @@
     , home-manager
     , disko
     , impermanence
-    }:
+    }:    
     let
-      inherit (nixpkgs-unstable) lib;
-      inputs =
+      inherit (nixpkgs-unstable) lib newScope;
+      pinned-sources = lib.mapAttrs
+        (_: input: input.outPath)
         { nixpkgs = nixpkgs-unstable ;
           inherit
             nixos-hardware
@@ -24,24 +25,13 @@
             disko
             impermanence ;
         };
-      sources =
-        (lib.mapAttrs
-          (_: input: input.outPath)
-          inputs) //
-        { overlay = ../overlay.nix; };
+    in lib.makeScope newScope (self:
+      { sources = pinned-sources // { overlay = ../overlay.nix; };
       
-      overlay-pinned = import ../overlay.nix sources;
-      overlay = import ../overlay.nix;
-      
-      nixpkgs-with-overlay =
-        import sources.nixpkgs
-          { overlays = [ overlay-pinned ]; };
-    in 
-      { inherit
-        inputs
-        sources
-        overlay
-        overlay-pinned
-        nixpkgs-with-overlay ;
-      };
+        overlay = import self.sources.overlay;
+        overlay-pinned = import self.sources.overlay self.sources;
+        
+        nixpkgs-with-overlay = import self.sources.nixpkgs
+          { overlays = [ self.overlay-pinned ]; };
+      });
 }
